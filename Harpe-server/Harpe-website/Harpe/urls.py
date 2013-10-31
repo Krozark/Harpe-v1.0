3862676fdeb0b5266d5b96a12a1ca525aa24cb4e
Krozark <maxime.barbier1991@gmail.com>
2013-10-31 14:02:10 +0100

    rm registration module

--- Harpe-server/Harpe-website/Harpe/urls.py
+++ Harpe-server/Harpe-website/Harpe/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     # Examples:
-    (r'^accounts/', include('registration.urls')),
     url(r'^', include('website.urls')),
     url(r'^', include('Kraggne.urls')),
 )
