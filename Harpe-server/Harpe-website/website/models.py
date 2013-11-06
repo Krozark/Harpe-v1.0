875308cfb23b9a690697edd5294ee001538379cd
Krozark <maxime.barbier1991@gmail.com>
2013-11-06 19:00:46 +0100

    add peptides models

--- Harpe-server/Harpe-website/website/models.py
+++ Harpe-server/Harpe-website/website/models.py
@@ -56,11 +56,14 @@ class Enzyme(models.Model):
 class AnalyseMgf(models.Model):
     owner      = models.ForeignKey(User)
     name       = models.CharField(_("name"),max_length=255,unique=True)
-    descriptif = models.TextField(_("Descriptif"),blank=True)
-    created    = models.DateTimeField(_("Created"),auto_now=True)
+    enzyme     = models.ForeignKey(Enzyme,blank=True,null=True)
+
     mgf        = models.FileField(_("MGF"),upload_to="mgf/")
 
-    paginate_by = 3
+    descriptif = models.TextField(_("Descriptif"),blank=True)
+    created    = models.DateTimeField(_("Created"),auto_now=True)
+
+    paginate_by = 5
 
     class Meta:
         ordering = ["-created",]
@@ -76,7 +79,28 @@ class AnalyseMgf(models.Model):
 
     def get_absolute_url(self):
         return reverse("analyse-detail",kwargs={"pk":self.pk})
-
 post_delete.connect(file_cleanup, sender=AnalyseMgf, dispatch_uid="AnalyseMgf.file_cleanup")
 
+
+class AnalysePeptide(models.Model):
+    analyse     = models.ForeignKey(AnalyseMgf,null=False,blank=False)
+    name        = models.CharField(_("name"),max_length=255,null=True,blank=True)
+    mgf_part    = models.TextField(_("Peptide MGF"),blank=False,null=False)
+
+    def __unicode__(self):
+        return "%s : %s" % (self.analyse,self.name or self.pk)
+
+    def get_absolute_url(self):
+        return "#"
+
+class PeptideFind(models.Model):
+    score       = models.FloatField(_("Score"),null=False,blank=False)
+    sequence    = models.TextField(_("Séquence"),null=False,blank=False,help_text = u"peak_masse(AA_id,peak_masse)*")
+    analyse     = models.ForeignKey(AnalysePeptide,null=False,blank=False)
+
+    def __unicode__(self):
+        return "%s : %f" % (self.analyse.name, self.score)
+
+    def get_absolute_url(self):
+        return "#"
 
